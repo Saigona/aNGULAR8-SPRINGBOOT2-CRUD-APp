547bac29fd03c6a60fc11e1b6ef14bba6a3efdc4
Saigona <100681104+Saigona@users.noreply.github.com>
2020-09-14 07:08:05 +0000

    change color scheme

--- cmd/watch/main.go
+++ cmd/watch/main.go
@@ -60,3 +60,72 @@ func main() {
 	flag.Parse()
 
 	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{streamURL}
+	}
+
+	b := bot.NewBot()
+	w := stream.InitWorker()
+
+	ctx := context.Background()
+	ctx = logger.WithLogEntry(ctx, log)
+
+	ctx, ctxCancel := signal.NotifyContext(ctx,
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGTERM,
+		os.Interrupt, os.Kill,
+	)
+	defer func() {
+		ctxCancel()
+		log.Info("main exiting")
+	}()
+	rokuCB := roku.Run(ctx) // TODO add flag and support autolaunch on motion
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	if _, ok := w.(*worker.Child); !ok { // FIXME hacky
+		if b != nil {
+			g.Go(func() error {
+				return b.Run(ctx)
+			})
+		} else {
+			log.Warn("twitter unconfigured")
+		}
+	}
+
+	for _, arg := range args {
+		log := log
+		if len(args) > 1 {
+			log = log.WithField("playlist", arg)
+		} else {
+			log = log.WithFields(logrus.Fields{})
+		}
+		ctx := logger.WithLogEntry(ctx, log)
+
+		u, err := url.Parse(arg)
+
+		if err != nil || u.Scheme == "" {
+			log.WithError(err).Fatalf("url.Parse: %s", arg)
+		}
+		s, err := stream.NewStream(
+			stream.WithFlags(),
+			stream.WithURL(u),
+			stream.WithWorker(w),
+			stream.WithRokuCB(rokuCB),
+			stream.WithBot(b),
+		)
+		if err != nil {
+			log.WithError(err).Fatal("stream.NewStream")
+		}
+		log.Info("monitoring")
+
+		g.Go(func() error {
+			return s.Run(ctx)
+		})
+		currentStream = s
+	}
+	go scanKeys(ctx)
+
+	if err := g.Wait(); err != nil {
+		log.Error(err)
+	}
+}
