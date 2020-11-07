47feddef759f4fdba40eb89ba8ac009901a34370
Saigona <100681104+Saigona@users.noreply.github.com>
2020-11-07 19:17:18 +0000

    Update INSTALLATION.md

--- internal/bot/post.go
+++ internal/bot/post.go
@@ -50,3 +50,74 @@ func (b *Bot) maybeDoPost(ctx context.Context, srcImages []imageRecord) ([]image
 		log.WithField("num_images", len(srcImages)).Info("not enough images to post")
 		return srcImages, nil
 	}
+
+	if age := time.Now().Sub(srcImages[0].time); age < minAge {
+		log.WithField("num_images", len(srcImages)).
+			WithField("age", age).
+			Info("images are too fresh too consider posting")
+		return srcImages, nil
+	}
+
+	// Try to pick a good spread of images
+	offset := len(srcImages) / numImages
+	images := make([]imageRecord, 0, numImages)
+	for i := 0; i < numImages; i++ {
+		img := srcImages[i*offset+(offset/2)]
+		images = append(images, img)
+	}
+
+	params := &twitter.StatusUpdateParams{
+		MediaIds: make([]int64, len(images)),
+	}
+
+	if time.Now().Sub(b.lastPosted) < replyWindow {
+		params.InReplyToStatusID = b.lastID
+	}
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	log.Info("uploading pics")
+	// TODO retry
+	for i, img := range images {
+		img := img
+		i := i
+		g.Go(func() error {
+			f := &bytes.Buffer{}
+			err := png.Encode(f, img.image)
+			if err != nil {
+				return errors.Wrap(err, "png.Encode")
+			}
+			media, _, err := b.client.Media.Upload(f.Bytes(), mimeType)
+			if err != nil {
+				return errors.Wrap(err, "Media.Upload")
+			}
+			params.MediaIds[i] = media.MediaID
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return srcImages, err
+	}
+
+	log.Info("posting status")
+	status := ""
+
+	if params.InReplyToStatusID == 0 {
+		n := time.Now().In(loc)
+		status = fmt.Sprintf("It's currently %s in Pyongyang & KCTV is on the air!", n.Format(time.Kitchen))
+	}
+
+	params.Status = status
+	// TODO retry
+	t, _, err := b.client.Statuses.Update(status, params)
+	if err != nil {
+		return srcImages, errors.Wrap(err, "Statuses.Update")
+	}
+
+	// These are racey TODO
+	b.lastPosted = time.Now()
+	b.lastID = t.ID
+
+	return nil, nil
+}
