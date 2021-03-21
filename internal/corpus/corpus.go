4fd244030eb2b6b1bc7e41de89124e0ddd4b558d
Saigona <100681104+Saigona@users.noreply.github.com>
2021-03-21 08:54:18 +0000

    Apply fixes from StyleCI

--- internal/corpus/corpus.go
+++ internal/corpus/corpus.go
@@ -44,4 +44,8 @@ func LoadFS(path string) (*Corpus, error) {
 
 	err := filepath.Walk(
 		fmt.Sprintf("%s/images/%s", corpusRoot, path),
-		func(path string, info fs.FileInfo, err er
+		func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			return c.loadEntry(path, info)
