293680ea39a79a81a9dda48b028179b8673ae00f
Saigona <100681104+Saigona@users.noreply.github.com>
2021-04-25 19:00:19 +0000

    bundler

--- internal/filter/dist_test.go
+++ internal/filter/dist_test.go
@@ -11,4 +11,4 @@ import (
 )
 
 //go:generate sh -c "go test ./... -run '^$' -benchmem -bench . | tee benchresult.txt"
-//go:generate sh -c "git show :./benchresult.txt | go run golang.or
+//go:generate sh -c "git show :./benchresult.txt | go run golang.org/x/perf/cmd/benchstat -delta-test none -geomean /dev/stdin benchresult.tx
