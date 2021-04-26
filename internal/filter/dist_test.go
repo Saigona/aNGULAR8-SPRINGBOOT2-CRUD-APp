fdb6dc60f6f750fe98c390ca4fe56b1697262ab8
Saigona <100681104+Saigona@users.noreply.github.com>
2021-04-26 15:13:22 +0000

    refs update.

--- internal/filter/dist_test.go
+++ internal/filter/dist_test.go
@@ -11,4 +11,7 @@ import (
 )
 
 //go:generate sh -c "go test ./... -run '^$' -benchmem -bench . | tee benchresult.txt"
-//go:generate sh -c "git show :./benchresult.txt | go run golang.org/x/perf/cmd/benchstat -delta-test none -geomean /dev/stdin benchresult.tx
+//go:generate sh -c "git show :./benchresult.txt | go run golang.org/x/perf/cmd/benchstat -delta-test none -geomean /dev/stdin benchresult.txt | tee benchdiff.txt"
+
+func TestMinDistFromCorpus(t *testing.T) {
+	testPatterns, err := 
