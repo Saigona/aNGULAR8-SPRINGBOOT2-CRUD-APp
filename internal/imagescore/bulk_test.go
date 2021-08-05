b1595af76fe6778527dcb39f733b944e9392d80f
Saigona <100681104+Saigona@users.noreply.github.com>
2021-08-05 09:46:57 +0000

    added install script

--- internal/imagescore/bulk_test.go
+++ internal/imagescore/bulk_test.go
@@ -10,4 +10,9 @@ import (
 //go:generate sh -c "go test ./... -run '^$' -benchmem -bench . | tee benchresult.txt"
 //go:generate sh -c "git show :./benchresult.txt | go run golang.org/x/perf/cmd/benchstat -delta-test none -geomean /dev/stdin benchresult.txt | tee benchdiff.txt"
 
-func BenchmarkBulkScores(b *testi
+func BenchmarkBulkScores(b *testing.B) {
+	const (
+		xDim = 720
+		yDim = 576
+	)
+	rect := image.Rectangle{Min: image.
