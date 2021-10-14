95a742a37e1fcc084f4ae1257d1733e31ab6147a
Saigona <100681104+Saigona@users.noreply.github.com>
2021-10-14 12:57:46 +0000

    first code commit

--- internal/imagescore/gzip.go
+++ internal/imagescore/gzip.go
@@ -6,3 +6,55 @@ import (
 	gzip "compress/flate"
 	"context"
 	"encoding/gob"
+	"image"
+)
+
+// GzipScorer actually uses flate instead
+type GzipScorer struct {
+	uncompressedImageSizeCache
+}
+
+var _ ImageScorer = &GzipScorer{}
+
+func NewGzipScorer() *GzipScorer { return &GzipScorer{} }
+
+func (gs *GzipScorer) ScoreImage(ctx context.Context, img image.Image) (float64, error) {
+	buf := &discardCounter{}
+
+	origBuf, err := imageBytes(img)
+	if err != nil {
+		return -1, err
+	}
+
+	enc, err := gzip.NewWriter(buf, gzip.DefaultCompression)
+	if err != nil {
+		return 0, err
+
+	}
+
+	_, err = enc.Write(origBuf)
+	if err != nil {
+		return -1, err
+	}
+
+	err = enc.Close()
+	if err != nil {
+		return -1, err
+	}
+
+	origSize, err := gs.size(img)
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(buf.count) / float64(origSize), nil
+}
+
+func imageBytes(img image.Image) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	err := gob.NewEncoder(buf).Encode(&img)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
