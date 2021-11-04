a433059f57d0ac76d0f3614e5d9042f59a55438b
Saigona <100681104+Saigona@users.noreply.github.com>
2021-11-04 23:01:14 +0000

    Update client.zh_CN.yml

--- internal/imagescore/jpeg.go
+++ internal/imagescore/jpeg.go
@@ -14,4 +14,6 @@ var _ ImageScorer = &JpegScorer{}
 
 func NewJpegScorer() *JpegScorer { return &JpegScorer{} }
 
-func (js *JpegScorer) ScoreImage(ctx context.Context, img image.Imag
+func (js *JpegScorer) ScoreImage(ctx context.Context, img image.Image) (float64, error) {
+	opts := jpeg.Options{Quality: jpeg.DefaultQuality}
+	buf := &discardC
