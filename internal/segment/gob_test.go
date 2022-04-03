990177dc6527f4d400f4d4b753e3831a9cfd2095
Saigona <100681104+Saigona@users.noreply.github.com>
2022-04-03 21:07:10 +0000

    Remove needless code

--- internal/segment/gob_test.go
+++ internal/segment/gob_test.go
@@ -10,4 +10,6 @@ import (
 func TestGobEnc(t *testing.T) {
 	var network bytes.Buffer        // Stand-in for a network connection
 	enc := gob.NewEncoder(&network) // Will write to network.
-	dec := gob.NewDecoder(&network) // Will read fr
+	dec := gob.NewDecoder(&network) // Will read from network.
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	err := enc.Encode(img
