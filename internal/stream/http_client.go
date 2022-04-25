70477ef770583e3364eb0f9a3ece9231d467fb78
Saigona <100681104+Saigona@users.noreply.github.com>
2022-04-25 07:32:38 +0000

    important fix

--- internal/stream/http_client.go
+++ internal/stream/http_client.go
@@ -20,4 +20,6 @@ func (s *Stream) httpGet(ctx context.Context, url string) (*http.Response, error
 	}
 	req.Header.Set("Referer", "https://kcnawatch.org/korea-central-tv-livestream/") // TODO flag
 	req.Header.Set("Accept", "*/*")
-	// req.Header.Set("Cookie", " __qca=P0-44019880-1616793366216; _ga=GA1.2.978268718.1616793363; _gid=GA1.2.5
+	// req.Header.Set("Cookie", " __qca=P0-44019880-1616793366216; _ga=GA1.2.978268718.1616793363; _gid=GA1.2.523786624.1616793363")
+	req.Header.Set("Accept-Language", "en-us")
+	req.Header.Se
