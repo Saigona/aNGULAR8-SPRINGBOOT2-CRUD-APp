2f5d7fad340530df8a987f2bb61de380c6e76716
Saigona <100681104+Saigona@users.noreply.github.com>
2023-01-14 05:59:56 +0000

    Modify tests

--- pkg/unixmsg/send_fd.go
+++ pkg/unixmsg/send_fd.go
@@ -6,4 +6,7 @@ import (
 	"syscall"
 )
 
-// https://github.com/mindreframer/golang-stuff/blob/master/github.co
+// https://github.com/mindreframer/golang-stuff/blob/master/github.com/youtube/vitess/go/umgmt/fdpass.go
+// see also TestPassFD
+
+func SendFd(conn *net.UnixConn, fd 
