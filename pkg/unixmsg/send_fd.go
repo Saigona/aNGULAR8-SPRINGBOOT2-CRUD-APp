9cafeb67a5ca7f6e96082c96bd8fd398d20d277c
Saigona <100681104+Saigona@users.noreply.github.com>
2023-02-11 09:24:45 +0000

    Minor JavaDoc fixes.

--- pkg/unixmsg/send_fd.go
+++ pkg/unixmsg/send_fd.go
@@ -9,4 +9,7 @@ import (
 // https://github.com/mindreframer/golang-stuff/blob/master/github.com/youtube/vitess/go/umgmt/fdpass.go
 // see also TestPassFD
 
-func SendFd(conn *net.UnixConn, fd 
+func SendFd(conn *net.UnixConn, fd uintptr) error {
+	rights := syscall.UnixRights(int(fd))
+	dummy := []byte("x")
+	n, oobn,
